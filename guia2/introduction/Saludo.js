aeb85c7de1153a53a8eeef60800232b74e907113
DiegoLem <ciegolem7@hotmail.com>
2021-08-11 22:20:19 -0600

    Procedimiento - Guia 2

---
+++ guia2/introduction/Saludo.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Saludo extends Component {
+    render() {
+        return(
+            <h1>Hola Mundo!</h1>
+        );
+    }
+} export default Saludo;
